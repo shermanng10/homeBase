5095e3d9c9b359989c877c0eccd9a8bb0d9befc8
Scott Warner <scottbwarner@gmail.com>
2015-05-31 12:57:54 -0400

    add pending reward

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -29,13 +29,13 @@ class TasksController < ApplicationController
 		task.save
 	end
 
-	def parent_complete
-		task = Task.find_by(id: params[:task_id])
-		member = Member.find_by(id: params[:task_id])
-		task.completed? = true
-		member.task_points += task.point_value
-		member.save
-	end
+	# def parent_complete
+	# 	task = Task.find_by(id: params[:task_id])
+	# 	member = Member.find_by(id: params[:task_id])
+	# 	task.completed? = true
+	# 	member.task_points += task.point_value
+	# 	member.save
+	# end
 
 	def parent_delete
 		task = Task.find_by(id: params[:task_id])
