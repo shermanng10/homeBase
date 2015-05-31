e91f637794b14c31a7465ebd40edd24b1f6b2f1d
Tanya Trinkle <TTrinkle@users.noreply.github.com>
2015-05-30 20:05:27 -0400

    Adding paperclip config

--- app/models/member.rb
+++ app/models/member.rb
@@ -27,3 +27,10 @@ class Member < ActiveRecord::Base
   end
 
 end
+has_attached_file :download,
+                    :storage => :s3,
+                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+
+  def s3_credentials
+    {:bucket => "xxx", :access_key_id => "xxx", :secret_access_key => "xxx"}
+  end
