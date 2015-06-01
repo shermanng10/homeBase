49f6b29e9ee11ec51192bd0fa1cee2f6c82e7c8a
Tanya Trinkle <tanya.trinkle@gmail.com>
2015-06-01 13:54:56 -0400

    Fixing merge conflict

--- app/assets/javascripts/getFamilies.js
+++ app/assets/javascripts/getFamilies.js
@@ -3,38 +3,6 @@
 //   getFamilymembers();
 // });
 
-
-<<<<<<< HEAD
-var getFamilyname = function(){
-  $.ajax({
-    url: "http://localhost:3000/families.json",
-    type: 'get',
-    dataType: 'json'
-  }).done(function(response){
-    var familynameTemplate = $('#familyname-template').html();
-    var showName = function(name){
-    var familynameRender = Mustache.render(familynameTemplate, name);
-    $('.familyname-container').append(familynameRender);
-    };
-    showName(response)
-    });
-};
-
-
-var getFamilymembers = function(){
-  $.ajax({
-    url: "http://localhost:3000/families.json",
-    type: 'get',
-    dataType: 'json'
-  }).done(function(response){
-    var memberMedalTemplate = $('#memberMedal-template').html();
-    response.members.forEach(function(member){
-      var memberMedalRender = Mustache.render(memberMedalTemplate, member);
-      $('.members-medals-container').append(memberMedalRender);
-    })
-    });
-};
-=======
 // var getFamilyname = function(){
 //   $.ajax({
 //     url: "http://localhost:3000/families.json",
@@ -66,4 +34,3 @@ var getFamilymembers = function(){
 //     })
 //     });
 // };
->>>>>>> Ability for parents to add points
