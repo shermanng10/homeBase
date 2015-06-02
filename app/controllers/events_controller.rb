cf27b8b2e7d31d94871216817d2c073ab579ec7b
fishermanng <sherman.ng10@gmail.com>
2015-06-02 11:30:33 -0400

    Add event controller and jsonbuilder

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,17 @@
+class EventsController < ApplicationController
+
+  def index
+    @events = Event.where(family_id: 1)
+  end
+
+  def create
+    @event= Event.new(event_params)
+    @event.save
+    redirect_to :back
+  end
+
+  private
+  def event_params
+    params.require(:event).permit :title, :start_time, :end_time, :family_id, :member_id
+  end
+end
