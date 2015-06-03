5b9fac9bd43b27c1b628d5842e479152b8a7931b
Siddharth Watal <sid.watal@gmail.com>
2015-06-03 15:25:57 -0400

    minor edits

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,8 +1,6 @@
 class Member < ActiveRecord::Base
 
-  # has_attached_file :img_url, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  # validates_attachment_content_type :img_url, :content_type => /\Aimage\/.*\Z/
-  belongs_to :family
+  
   has_many :events, dependent: :destroy
   has_many :rewards, dependent: :destroy
   has_many :assigned_tasks, foreign_key: :assigned_member_id, class_name: "Task", dependent: :destroy
@@ -11,14 +9,12 @@ class Member < ActiveRecord::Base
   validates :name, :family, :img_url, presence: true
   validates :task_points, numericality: {only_integer: true}
 
-  # validates_attachment_content_type :img_url, :content_type => /\Aimage\/.*\Z/
-
   def tasks_left_to_do
     self.assigned_tasks - self.completed_tasks.where(complete: true)
   end
 
   def assign_color
-    colors = ["#7E57C2", "#42A5F5", "#26A69A", "#FF9800", "#FFEE58", "#78909C", "#EC407A", "#AB47BC", "#26C6DA", "#8BC34A", "#FFCA28", "#69F0AE"]
+    colors = ["#1abc9c",  "#e67e22", "#2980b9", "#f1c40f", "#9b59b6", "#34495e", "#d35400", "#FFB6C1", "#FFCA28", "#69F0AE"]
     count = self.family.members.length + 1
     self.color = colors[count]
   end
