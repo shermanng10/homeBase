0c4028b9f09b5feca6f0ac30889758a5a68012c8
fishermanng <sherman.ng10@gmail.com>
2015-06-15 12:14:01 -0400

    Fixes application module

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -1,17 +1,17 @@
-// $('document').ready(function() {
-//   getWeather();
-// })
+$('document').ready(function() {
+  getWeather();
+})
 
-// var getWeather = function(){
+var getWeather = function(){
 
-//   $.ajax({
-//     url: "http://api.openweathermap.org/data/2.5/weather?zip=10008,us&units=imperial",
-//     method: "GET",
-//   }).done(function(response){
-//     temp = response.main.temp
-//     description = response.weather[0].description
-//     $('#weather').text(temp + " and " + description)
-//   }).fail(function(error){
-//     console.log(error)
-//   })
-// }
+  $.ajax({
+    url: "http://api.openweathermap.org/data/2.5/weather?zip=10008,us&units=imperial",
+    method: "GET",
+  }).done(function(response){
+    temp = response.main.temp
+    description = response.weather[0].description
+    $('#weather').text(temp + " and " + description)
+  }).fail(function(error){
+    console.log(error)
+  })
+}
